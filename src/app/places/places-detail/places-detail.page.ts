cfa9e2fe4800ab09003dfe4c8ffa0fe93a58b65f
elias-sanchez <e.sanchez.arroyo91@gmail.com>
2020-12-05 13:10:41 +0100

    sesion 2 - min 49:04

--- src/app/places/places-detail/places-detail.page.ts
+++ src/app/places/places-detail/places-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-places-detail',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesDetailPage implements OnInit {
 
-  constructor() { }
+  place: Place
+
+  constructor(private activatedRoute: ActivatedRoute, private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap =>{
+      // redirect
+      const recipeId = paramMap.get('placeId')
+      this.place = this.placesService.getPlace(recipeId);
+      console.log(this.place);
+    })
   }
 
 }
